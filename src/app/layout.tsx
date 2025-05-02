e55f4862708a09f0655a6f77484bae499a26ad4b
Amos Lim <amoseui@gmail.com>
2025-05-02 20:31:12 +0900

    fix lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import React from "react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
